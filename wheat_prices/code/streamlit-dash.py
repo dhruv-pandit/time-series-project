11c092ff15507aeecdb51ac7c624c5648a55a3a1
Dhruv Pandit <78376173+dhruv-pandit@users.noreply.github.com>
2023-10-30 10:37:39 +0000

    changes

--- wheat_prices/code/streamlit-dash.py
+++ wheat_prices/code/streamlit-dash.py
@@ -12,7 +12,7 @@ from prophet import Prophet
 @st.cache_data
 def load_data():
     url = 'https://github.com/dhruv-pandit/time-series-project/blob/33d707e837449a341a05f576f928f891fbd4f696/wheat_prices/Datasets/PWHEAMTUSDM.xls'
-    df_wheat = pd.read_excel(url, skiprows=10).rename(columns={'PWHEAMTUSDM' : 'Wheat_Price'})
+    df_wheat = pd.read_excel(url, skiprows=10, engine='xlrd').rename(columns={'PWHEAMTUSDM' : 'Wheat_Price'})
     return df_wheat
 
 # Check stationarity
